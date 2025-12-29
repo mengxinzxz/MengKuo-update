a0242c298779e97001983bd406283fde797d8084
mengxinzxz <2223529500@qq.com>
2025-12-29 22:05:19 +0800

    修复单骑无双模式先手胜利胜负奖励均翻倍的bug

--- js/content/bol_danqiwushuang.js
+++ js/content/bol_danqiwushuang.js
@@ -168,25 +168,23 @@ const brawl = {
                             }
                             if (ui.decade_ddzInfo) ui.decade_ddzInfo.innerHTML = '本局票数：' + game.max_beishu * 100;
                             lib.onover.push(bool => {
-                                var numx = game.max_beishu * 100;
+                                const num = game.max_beishu * 100;
                                 if (bool == undefined) {
                                     for (var i of game.filterPlayer2()) i.chat('+0');
                                 }
                                 else {
-                                    const player = game.me;
                                     if (game.zhu.isAlive()) {
                                         for (var i of game.filterPlayer2()) {
-                                            if (i == game.zhu) i.chat('+' + numx * 2);
-                                            else i.chat('-' + numx);
+                                            if (i == game.zhu) i.chat('+' + num * 2);
+                                            else i.chat('-' + num);
                                         }
                                     }
                                     if (!game.zhu.isAlive()) {
                                         for (var i of game.filterPlayer2()) {
-                                            if (i == game.zhu) i.chat('-' + numx * 2);
-                                            else i.chat('+' + numx);
+                                            if (i == game.zhu) i.chat('-' + num * 2);
+                                            else i.chat('+' + num);
                                         }
                                     }
-                                    const num = numx * (game.zhu === player ? 2 : 1);
                                     game.bol_say(`战斗${bool ? '胜利' : '失败'}，${bool ? '获得' : '失去'}${num}萌币`);
                                     game.saveConfig('extension_活动萌扩_decade_Coin', lib.config.extension_活动萌扩_decade_Coin + num * (bool ? 1 : -1));
                                 }
